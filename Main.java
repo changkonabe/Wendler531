3c4b4f5bc259bdfc16764cc9228de9b33b4f2959
Kevin Chang <changkevin@protonmail.ch>
2017-06-14 15:59:44 -0700

    Created files for Wendler531.

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+
+
+/** Main method for executing Wendler531
+ *  program, used to calculate working sets
+ *  for overhead press, bench, squat, and deadlift
+ *  based on user inputted 1 rep maxes.
+ *  @author K. Chang
+ */
+class Wendler531 {
+    public static void main(String... args) {
+        
+
+    }
+}
---
+++ Program.java
@@ -0,0 +1,7 @@
+
+/** Java class representing the 531 Program.
+ *  @author K. Chang
+ */
+class Program {
+
+}
